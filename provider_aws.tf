8ef356db2c1deaa62075486667583e01ec183a35
Productive Analytics <prod8ctive.analytics@gmail.com>
2019-07-13 04:10:43 -0500

    Working ECS-Fargate cluster w/ Application ALB

---
+++ provider_aws.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_version  = ">= 0.12.4"
+}
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.aws_region}"
+}
